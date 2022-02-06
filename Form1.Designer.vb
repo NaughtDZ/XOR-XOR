09f1a706912e09563b5fcd696d8353f3645ed88e
NaughtDZ <1603705420@qq.com>
2022-02-06 20:20:44 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.Location = New System.Drawing.Point(10, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(328, 72)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "本软件使用最最最最简单但是好用的XOR加密。如果你放入的文件是加密过的，那么处理后就是解密的，反之亦然！只要某些人不暴力计算密码，XOR也依旧是无法破解的！" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "相" &
+    "信我，度盘是靠MD5识别的，不会硬破解你的压缩包密码，所谓双压缩加密还被破解，仅仅只是因为这个被加密的资源依旧被人举报了。" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "RAR的普通加密是可以的，当然咯如" &
+    "果你的文件命过于奇怪，不封你封谁？"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.AllowDrop = True
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(12, 138)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(326, 304)
+        Me.ListBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(12, 84)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(222, 21)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "Password"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(11, 449)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(324, 20)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar2.Location = New System.Drawing.Point(12, 475)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(324, 20)
+        Me.ProgressBar2.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(242, 84)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 21)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "帮我随机密码"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Location = New System.Drawing.Point(12, 111)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(324, 21)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "把文件进来，点击此按钮开始加密/解密"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(347, 507)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "锟斤拷牌XOR文件加密解密工具"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,120 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        'Label1.Size = Me.ClientSize - New Size(Label1.Location)
+        'Button3.Size = Me.ClientSize - New Size(Button3.Location)
+        'ListBox1.Size = Me.ClientSize - New Size(ListBox1.Location)
+        'ProgressBar1.Size = Me.ClientSize - New Size(ProgressBar1.Location)
+        'ProgressBar2.Size = Me.ClientSize - New Size(ProgressBar2.Location)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("请牢记随机出来的密码！", MsgBoxStyle.Exclamation, "注意！")
+        Randomize(System.DateTime.Now.Ticks)
+        TextBox2.Text = Math.Round(Rnd() * 89999999999 + 1000000000)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ListBox1_DragDrop(sender As Object, e As DragEventArgs) Handles ListBox1.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop, False) Then
+            Dim dragedfiles As String() = CType(e.Data.GetData(DataFormats.FileDrop), String())
+            For Each s As String In dragedfiles
+                ListBox1.Items.Add(s)
+            Next
+        End If
+    End Sub
+
+    Private Sub ListBox1_DragEnter(sender As Object, e As DragEventArgs) Handles ListBox1.DragEnter
+        e.Effect = DragDropEffects.All
+    End Sub
+
+    Private Sub ListBox1_DoubleClick(sender As Object, e As EventArgs) Handles ListBox1.DoubleClick
+        Try
+            ListBox1.Items.RemoveAt(ListBox1.SelectedIndex)
+        Catch
+        End Try
+    End Sub
+
+    Private Sub ListBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles ListBox1.MouseUp
+        Timer1.Enabled = True '兜一圈子就是为了双击清除列表
+        Debug.WriteLine(listrightdulclick)
+        If e.Button = MouseButtons.Right And listrightdulclick > 0 Then
+            ListBox1.Items.Clear()
+        ElseIf e.Button <> MouseButtons.Right Then
+            Timer1.Enabled = False
+            listrightdulclick = 0
+        End If
+    End Sub
+    Dim listrightdulclick As Byte = 0
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        listrightdulclick += 1 'vb.net没有右键双击事件？？？
+        Debug.WriteLine(listrightdulclick)
+        If listrightdulclick >= 4 Then
+            listrightdulclick = 0
+            Timer1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Mainwork(filelocal As String, filesave As String, password As String)
+        Try
+            Dim fs As New IO.FileStream(filelocal, IO.FileMode.Open) '读
+            Dim fsave As New IO.FileStream(filesave, IO.FileMode.Create) '写
+            Dim bynow As Byte, byenkey As Byte
+            Dim passwordbyt = System.Text.Encoding.Unicode.GetBytes(password), passwordbyte_count As Integer, password_now_byte As Integer = 0 '密码大于一个字节的话，得换着字节去对应文件字节加密
+            passwordbyte_count = passwordbyt.Length - 1
+            ProgressBar2.Value = 0
+            ProgressBar2.Maximum = fs.Length '进度条
+            For i = 0 To fs.Length - 1
+                bynow = fs.ReadByte()
+                byenkey = BitConverter.GetBytes(bynow Xor passwordbyt(password_now_byte))(0) 'XOR加密/解密操作，加转换防止溢出BUG
+                If password_now_byte < passwordbyte_count Then
+                    password_now_byte += 1
+                Else
+                    passwordbyte_count = 0
+                End If
+                fsave.WriteByte(byenkey)
+                ProgressBar2.Value += 1
+            Next
+            fsave.Close()
+            fsave.Dispose()
+            fs.Close()
+            fs.Dispose()
+        Catch e As Exception
+            Debug.WriteLine(e.ToString)
+            Exit Sub
+        End Try
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox2.Text = "" Then
+            MsgBox("请输入密码", MsgBoxStyle.Critical, "没得密码？？！")
+            Exit Sub
+        End If
+        Dim xorfile As String
+        If ListBox1.Items.Count > 0 Then
+            ProgressBar1.Value = 0
+            ProgressBar1.Maximum = ListBox1.Items.Count
+            For Each files In ListBox1.Items
+                If InStr(IO.Path.GetFileName(files), "(_Xor_)", CompareMethod.Text) = 0 Then '添加或删去加密标记
+                    xorfile = IO.Path.GetDirectoryName(files) & "\(_Xor_)" & IO.Path.GetFileName(files)
+                Else
+                    xorfile = Replace(files.ToString, "(_Xor_)", "",,, CompareMethod.Text)
+                End If
+                Mainwork(files, xorfile, TextBox2.Text)
+                ProgressBar1.Value += 1
+            Next
+        End If
+    End Sub
+End Class
+
+
+
