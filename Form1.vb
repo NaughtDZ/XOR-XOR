12f5b40698c07cdcadac83e2fcaab5c746a3b3ef
NaughtDZ <1603705420@qq.com>
2022-02-08 19:36:50 +0800

    git config --global --unset http.proxy

--- Form1.vb
+++ Form1.vb
@@ -56,27 +56,51 @@
     ''' <summary>
     ''' 对文件进行处理和调用加密
     ''' </summary>
-    ''' <param name="config"></param>
-    Private Sub Mainwork(config As Object()) 'filelocal As String, filesave As String, password As String
+    ''' <param name="config"> 'filelocal As String, filesave As String, password As String</param>
+    Private Sub Mainwork(config As Object())
         Try
-
-            Dim fsave As New IO.FileStream(config(1), IO.FileMode.Create) '写
-            Dim fs As Byte(), opened_file As IO.FileStream
-            Dim split_procce As New Xor_work
-            opened_file = IO.File.OpenRead(config(0))
-            If opened_file.Length > 8 Then
-                If opened_file.Length Mod 8 = 0 Then
-                    For x = 0 To 7
-                        Dim parameter() As Object = {1, 1, 1, 1}
-                        split_procce(Add
+            Dim fsave As New IO.FileStream(config(1), IO.FileMode.Create) '备写文件
+            Dim opened_file As IO.FileStream = IO.File.OpenRead(config(0))  '读取的文件
+            Dim run_thread(7) As Threading.Thread '单个文件拆8个线程
+            Dim split_procce(7) As Xor_work '8个处理类
+            Dim x1 As Long = 0, x2 As Long = 0, x3() As Byte, x4 As String = config(2), parameter(2) As Object 'bystar As Long, count As Long, x3 As Byte(), password As String
+            If opened_file.Length > 8 Then '文件可以被拆分，事实上，真的有人会加密8个字节都没有的文件？？？
+                If opened_file.Length Mod 8 = 0 Then '文件恰好可以被拆成8个字节相等的部分,该程序这么判定是因为不想自作主张往你的文件里写00，这样会导致解密后文件md5可能和原始文件对不上
+                    '解决是否文件恰好可以被拆成8个字节相等的部分,该程序这么判定是因为不想自作主张往你的文件里写00，这样会导致解密后文件md5可能和原始文件对不上
+                    x2 = (opened_file.Length - opened_file.Length Mod 8) / 8 '确定分段长度
+                    ReDim x3(x2 - 1)
+                    For fill = 0 To x2 - 1
+                        x3(fill) = 0
                     Next
-                    For i = 0 To opened_file.Length - 7 Step opened_file.Length / 8 '分8组，但是最后一组只要从开头就ok，所以要减去后面的
-
-
+                    For n1 = 0 To 6
+                        opened_file.Read(x3, x1, x2) '分段加密
+                        parameter(0) = x2 : parameter(1) = x3 : parameter(2) = x4 '准备好线程中要传递参数
+                        run_thread(n1) = New Threading.Thread(AddressOf split_procce(n1).xor_working) '创建当前分段线程
+                        run_thread(n1).Start(parameter) '运行当前分段线程
+                        x1 += (opened_file.Length - opened_file.Length Mod 8) / 8 '移动到下一段
                     Next
-                Else
-
+                    '单独处理最后一份
+                    x2 = ((opened_file.Length - opened_file.Length Mod 8) / 8) + opened_file.Length Mod 8 '确定分段长度
+                    ReDim x3(x2 - 1)
+                    For fill = 0 To x2 - 1
+                        x3(fill) = 0
+                    Next
+                    opened_file.Read(x3, x1, x2) '赋值给x3需要分段加密内容
+                    parameter(0) = x2 : parameter(1) = x3 : parameter(2) = x4 '准备好线程中要传递参数
+                    run_thread(7) = New Threading.Thread(AddressOf split_procce(7).xor_working) '创建当前分段线程
+                    run_thread(7).Start(parameter) '运行当前分段线程
                 End If
+                'run_thread(0).Join() : run_thread(1).Join() : run_thread(2).Join() : run_thread(3).Join() : run_thread(4).Join() : run_thread(5).Join() : run_thread(6).Join() : run_thread(7).Join()
+                For n2 = 1 To 7
+                    split_procce(0).main_result.AddRange(split_procce(n2).main_result)
+                Next
+                fsave.Write(split_procce(0).main_result.ToArray(Type.GetType("System.Byte")), 0, opened_file.Length)
+            Else '做个人吧，8个字节都不到的文件你还要加密？？？
+                Dim onetime_ok As New Xor_work
+                Dim onece() As Object = {opened_file.Length, IO.File.ReadAllBytes(config(0)), config(2)}
+                onetime_ok.xor_working(onece)
+                Dim be_write As Byte() = onetime_ok.main_result.ToArray(Type.GetType("System.Byte"))
+                fsave.Write(be_write, 0, be_write.Length)
             End If
             '------------------------------------老的单线程处理
             'Dim fs As New IO.FileStream(config(0), IO.FileMode.Open) '读
@@ -130,25 +154,26 @@ End Class
 ''' 曲线救国的多线程分段xor加密，方便获得处理结果
 ''' </summary>
 Class Xor_work
-    Dim main_result As ArrayList
+    Friend main_result As ArrayList
     ''' <summary>
     ''' XOR加密过程
     ''' </summary>
+    ''' <param name="paramer"> count As Long, x3 As Byte(), password As String</param>
     ''' <returns>返回的是加密后的字节组</returns>
-    Function xor_working(bystar As Long, count As Long, fs As Byte(), password As String) As ArrayList
-        Dim passwordbyt = System.Text.Encoding.Unicode.GetBytes(password), passwordbyte_count As Integer, password_now_byte As Integer = 0 '密码大于一个字节的话，得换着字节去对应文件字节加密
-        Dim byenkey As Byte, result As ArrayList
+    Function xor_working(paramer As Object()) As ArrayList
+        Dim passwordbyt = System.Text.Encoding.Unicode.GetBytes(paramer(2)), passwordbyte_count As Integer, password_now_byte As Integer = 0 '密码大于一个字节的话，得换着字节去对应文件字节加密
+        Dim byenkey As Byte, result As New ArrayList
         passwordbyte_count = passwordbyt.Length - 1
-        For i = 0 To count '拆分工作和计数工作应该在主任务里就完成了，在子线程拆分会导致爆内存?
-            byenkey = BitConverter.GetBytes(fs(i) Xor passwordbyt(password_now_byte))(0) 'XOR加密/解密操作，加转换防止溢出BUG
+        For i = 0 To paramer(0) - 1 '拆分工作和计数工作应该在主任务里就完成了
+            byenkey = BitConverter.GetBytes(paramer(1)(i) Xor passwordbyt(password_now_byte))(0) 'XOR加密/解密操作，加转换防止溢出BUG
             If password_now_byte < passwordbyte_count Then
                 password_now_byte += 1
             Else
                 password_now_byte = 0
             End If
             result.Add(byenkey)
-            main_result = result
         Next
+        main_result = result
         Return result '本函数是为了上门的拆分的
     End Function
 End Class
